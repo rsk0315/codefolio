96632a77fb86bb89b640e481858584001f2acda1
rsk0315 <rsk0315@icloud.com>
2018-08-23 17:37:38 +0900

    Do something with codeblocks

--- js/codeblock.js
+++ js/codeblock.js
@@ -1,29 +1,40 @@
 $(window).on('load', function() {
     $('.cmd-in, .cmd-cont').prepend($('<span class="space">'));
-    // $('.cmd-out').attr({
-    //     'data-content': $(this),
-    // });
     $.each($('.cmd-out'), function() {
         var text = $(this).text();
         $(this).attr({'data-content': text});
         $(this).text('');
     });
 
+    $('.cmd-in').before($('<span class="ps1">'));
+    $('.cmd-root').prev().addClass('ps1-root');
+    if (document.queryCommandSupported('copy')) {
+        $('.ps1').attr({
+            'data-toggle': 'tooltip',
+            'data-trigger': 'manual',
+            title: 'Copied!',
+        });
+    }
+
     $('.ps1').click(function() {
         window.getSelection().removeAllRanges();
         try {
             var range = document.createRange();
-            range.selectNode($('#'+$(this).data('target')).get(0));
+            var selected = $(this).next()[0];
+            // console.log(selected);
+            range.selectNode(selected);
             window.getSelection().addRange(range);
             document.execCommand('copy');
             $(this).tooltip('show');
             var _this = this;
             setTimeout(function() { $(_this).tooltip('hide'); }, 800);
+
+            // remove selection from tooltip
+            range.selectNode(selected);
         } catch (err) {
             console.log(err);
         }
-        window.getSelection().removeAllRanges();
     });
 
-    $('[data-toggle="tooltip"]').tooltip();
+    $('.ps1').tooltip();
 });
