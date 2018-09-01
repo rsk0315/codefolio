098adea0cba4790f73ea774f4761bd33f441c0a2
rsk0315 <rsk0315@icloud.com>
2018-09-01 16:18:38 +0900

    Add a tuple-like scanning

--- Rust/scanner.rs
+++ Rust/scanner.rs
@@ -21,3 +21,20 @@ fn scan_split_with<T: std::str::FromStr>(ch: char) -> Vec<T> {
 fn scan_lines<T: std::str::FromStr>(n: usize) -> Vec<T> {
     (0..n).map(|_| scan::<T>()).collect()
 }
+
+macro_rules! scan_as_tuple {
+    ( $t:ty ) => {
+        (scan::<$t>(),)
+    };
+    ( $( $t:ty ),* ) => {
+        {
+            let mut s = String::new();
+            std::io::stdin().read_line(&mut s).ok();
+            let v = s.trim().split_whitespace();
+            let mut it = v;
+            let ret = ( $(it.next().unwrap().parse::<$t>().ok().unwrap()),* );
+            assert!(it.next() == None, "not all tokens consumed");
+            ret
+        }
+    };
+}
