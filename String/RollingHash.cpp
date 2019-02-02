d5b95424ec5bd35839711dcad45a5e1933f89e6c
rsk0315 <rsk0315@icloud.com>
2019-02-03 02:05:23 +0900

    Add member functions

--- String/RollingHash.cpp
+++ String/RollingHash.cpp
@@ -1,9 +1,9 @@
 template <intmax_t X=943681729, intmax_t MOD=1000000007>
-class RollingHash {
+class rolling_hash {
   std::vector<intmax_t> hash;
   intmax_t offset(size_t len) const {
-    intmax_t res=1;
-    for (intmax_t dbl=X%MOD; len; len>>=1) {
+    intmax_t res = 1;
+    for (intmax_t dbl = X%MOD; len; len >>= 1) {
       if (len & 1)
         res = res*dbl % MOD;
 
@@ -14,10 +14,13 @@ class RollingHash {
   }
 
 public:
-  RollingHash(const std::string &s): hash(s.length()+1) {
-    for (size_t i=0; i<s.length(); ++i)
+  rolling_hash(const std::string &s): hash(s.length()+1) {
+    for (size_t i = 0; i < s.length(); ++i)
       hash[i+1] = (hash[i]*X+s[i]) % MOD;
   }
+
+  intmax_t get_base() const { return X; }
+  intmax_t get_mod() const { return MOD; }
 
   intmax_t prefix(size_t i) const {
     // hash of prefix s[0..i-1] (inclusive)
@@ -31,6 +34,15 @@ public:
     intmax_t res=hash[j];
     res = (res - offset(j-i)*hash[i]) % MOD;
     if (res < 0) res += MOD;
+    return res;
+  }
+
+  intmax_t inserted(size_t i, int ch) const {
+    intmax_t res = 0;
+    if (i > 0) res += prefix(i);
+    res = (res*X + ch) % MOD;
+    size_t n = hash.size()-1;
+    if (i < n) res = (res*offset(n-i) + substr(i, n)) % MOD;
     return res;
   }
 
