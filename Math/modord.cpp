f52d37afc57c146b5d9572960551907cfe769e13
rsk0315 <rsk0315@icloud.com>
2019-03-15 18:35:34 +0900

    Add a snippet

---
+++ Math/modord.cpp
@@ -0,0 +1,12 @@
+template <class Tp>
+intmax_ ord(Tp g, Tp p, const std::map<Tp, intmax_t>& f) {
+  intmax_t q = p-1;
+  for (const auto& pp: f) {
+    for (intmax_t i = 0; i < pp.second; ++i) {
+      Tp e = pp.first;
+      if (modpow(g, q/e, p) == 1)
+        q /= e;
+    }
+  }
+  return q;
+}
