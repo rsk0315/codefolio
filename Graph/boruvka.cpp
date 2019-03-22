7f48e1b4a1edc1bc07d9fae09f45c7638b07037b
rsk0315 <rsk0315@icloud.com>
2019-03-23 00:12:45 +0900

    Reduce spaces by storing indices

--- Graph/boruvka.cpp
+++ Graph/boruvka.cpp
@@ -6,16 +6,22 @@ Weight min_spanning_tree(const graph<Weight>& g) {
   intmax_t res = 0;
   union_find uf(v);
   while (uf.size(0) < v) {
-    std::vector<edge<Weight>> min(v, edge<Weight>(-1, -1, inf<Weight>));
+    std::vector<std::pair<size_t, size_t>> min(v, {-1, -1});
     for (size_t i = 0; i < v; ++i)
-      for (const auto& e: g[i]) {
+      for (size_t j = 0; j < g[i].size(); ++j) {
+        const auto& e = g[i][j];
         if (uf.connected(e.src, e.dst)) continue;
         size_t s = uf.find(e.src);
-        if (min[s].cost > e.cost) min[s] = e;
+        size_t u0, u1;
+        std::tie(u0, u1) = min[s];
+        if (u0+1 == 0 || g[u0][u1].cost > e.cost) min[s] = {i, j};
       }
 
-    for (const auto& e: min) {
-      if (e.src+1 == 0) continue;
+    for (size_t i = 0; i < v; ++i) {
+      size_t u0, u1;
+      std::tie(u0, u1) = min[i];
+      if (u0+1 == 0) continue;
+      const auto& e = g[u0][u1];
       if (uf.connected(e.src, e.dst)) continue;
       uf.unite(e.src, e.dst);
       res += e.cost;
