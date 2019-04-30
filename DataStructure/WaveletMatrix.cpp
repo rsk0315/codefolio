1b807333f502ec9445cd7d7105b1396fdc5ccf5b
rsk0315 <rsk0315@icloud.com>
2019-05-01 00:45:09 +0900

    Add WM

--- DataStructure/WaveletMatrix.cpp
+++ DataStructure/WaveletMatrix.cpp
@@ -1,160 +1,355 @@
-#include <cstdio>
-#include <cstdint>
-#include <vector>
-#include <algorithm>
-#include <array>
+class bit_vector {
+  size_t n;
+  static constexpr size_t nbit = 64;
+  std::vector<intmax_t> raw;
+  std::vector<int> acc;
 
-#include <random>
-
-class BitVector {
-  size_t n, lg_n;
-  size_t large_width, small_width, pop_width;
-  // small を <int> で実装するなと書いてあるが？
-  std::vector<size_t> small, popcount;
-  std::vector<int> original;
-
-  size_t lg(size_t n) const {
-    return n? 63-__builtin_clzll(n):0;
+  int popcount(uintmax_t x) const {
+    return __builtin_popcountll(x);
   }
 
 public:
-  BitVector(): n(0), lg_n(0), small(), popcount() {}
+  bit_vector() {}
 
-  BitVector(const std::vector<bool> &v):
-    // ここの n が怖くて，適当に切り上げる必要がありそう
-    n(v.size()), lg_n(lg(n)),
-    small_width(lg_n/2), pop_width(1<<small_width),
-    small(n/small_width), popcount(pop_width),
-    original(n/small_width)
-  {
-    // /* SMALL */
-    for (size_t i=0; i+small_width<n; ++i)
-      // ここ二重ループにするとかで除算を回避したい
-      if (v[i]) ++small[i/small_width+1];
+  bit_vector(const std::vector<bool>& b): n(b.size()) {
+    raw.assign(n/nbit+1, 0);
+    for (size_t i = 0; i < n; ++i)
+      if (b[i]) raw[i/nbit] |= intmax_t(1) << (i % nbit);
 
-    for (size_t i=1; i<n/small_width; ++i)
-      small[i] += small[i-1];
-
-    /* POP-COUNT */
-    for (size_t i=0; i<pop_width; ++i)
-      popcount[i] = __builtin_popcount(i);
-
-    /* ORIGINAL */
-    for (size_t i=0; i<n; i+=small_width)
-      for (size_t j=0; j<small_width; ++j)
-        if (v[i+j]) original[i/small_width] |= 1<<j;
+    acc.assign(n/nbit+1, 0);
+    for (size_t i = 1; i < acc.size(); ++i)
+      acc[i] = acc[i-1] + popcount(raw[i-1]);
   }
 
-  size_t rank(size_t k) const {
-    if (k-- == 0) return 0;
-    int res=popcount[original[k/small_width]&((1<<(k%small_width+1))-1)];
-    res += small[k/small_width];
+  size_t rank1(size_t k) const {
+    size_t large = k / nbit;
+    size_t small = k % nbit;
+    size_t res = acc[large];
+    if (small > 0) res += popcount(raw[large] & ((uintmax_t(1) << small) - 1));
     return res;
   }
 
-  size_t select(size_t m) const {
-    if (m == 0) return 0;
+  size_t rank0(size_t k) const {
+    return k - rank1(k);
+  }
 
-    size_t lb=0, ub=small.size();
+  size_t select1(size_t k) const {
+    if (k == 0) return 0;
+    size_t lb = 0;
+    size_t ub = n;
     while (ub-lb > 1) {
-      size_t mid=(lb+ub)>>1;
-      (small[mid]<m? lb:ub) = mid;
+      size_t mid = (lb+ub) >> 1;
+      ((rank1(mid) < k)? lb:ub) = mid;
     }
-    for (size_t i=0; i<small_width; ++i) {
-      if (original[lb] >> i & 1) {
-        if (--m == small[lb])
-          return lb*small_width+i+1;
-      }
-    }
+    if (rank1(ub) < k) return -1;
+    return ub;
+  }
 
-    return -1;
+  size_t select0(size_t k) const {
+    if (k == 0) return 0;
+    size_t lb = 0;
+    size_t ub = n;
+    while (ub-lb > 1) {
+      size_t mid = (lb+ub) >> 1;
+      ((rank0(mid) < k)? lb:ub) = mid;
+    }
+    if (rank0(ub) < k) return -1;
+    return ub;
+  }
+
+  size_t rank(int x, size_t k) const {
+    return x? rank1(k) : rank0(k);
+  }
+
+  size_t select(int x, size_t k) const {
+    return x? select1(k) : select0(k);
+  }
+
+  bool operator [](size_t k) const {
+    size_t large = k / nbit;
+    size_t small = k % nbit;
+    return raw[large] >> small & 1;
   }
 };
 
-template <class Integer>
-class WaveletMatrix {
-  std::vector<Integer> entity;
-  static constexpr size_t BIT_SIZE=8*sizeof(Integer);
-  std::array<BitVector, BIT_SIZE> bv;
+template <class Tp, size_t bitlen = 8 * sizeof(Tp)>
+class wavelet_matrix {
+public:
+  using value_type = std::make_unsigned<Tp>;
+
+private:
+  std::vector<value_type> c;
+  std::vector<size_t> zeros;
+  size_t n;
+  std::array<bit_vector, bitlen> a;
+
+  size_t start_index(value_type x) const {
+    size_t s = 0;
+    size_t t = 0;
+    for (size_t i = bitlen; i-- > 1;) {
+      size_t j = bitlen-i-1;
+      if (x >> i & 1) {
+        s = zeros[j] + a[j].rank1(s);
+        t = zeros[j] + a[j].rank1(t);
+      } else {
+        s = a[j].rank0(s);
+        t = a[j].rank0(t);
+      }
+    }
+    return s;
+  }
 
 public:
   template <class ForwardIt>
-  WaveletMatrix(ForwardIt first, ForwardIt last) {
-    size_t n=last-first;
-    std::vector<Integer> vi(first, last);
-    for (size_t i=BIT_SIZE; i--;) {
-      std::vector<Integer> one, zero;
+  wavelet_matrix(ForwardIt first, ForwardIt last):
+    c(first, last), zeros(bitlen), n(c.size())
+  {
+    std::vector<value_type> whole = c;
+    for (size_t i = bitlen; i--;) {
+      std::vector<value_type> zero, one;
       std::vector<bool> vb(n);
-      for (size_t j=0; j<vi.size(); ++j) {
-        if (vi[j]>>i & 1) {
-          vb[j] = true;
-          one.push_back(vi[j]);
-        } else {
-          zero.push_back(vi[j]);
+      for (size_t j = 0; j < n; ++j) {
+        ((whole[j] >> i & 1)? one:zero).push_back(whole[j]);
+        vb[j] = (whole[j] >> i & 1);
+      }
+
+      zeros[bitlen-i-1] = zero.size();
+      a[bitlen-i-1] = bit_vector(vb);
+      if (i == 0) break;
+      whole = std::move(zero);
+      whole.insert(whole.end(), one.begin(), one.end());
+    }
+  }
+
+  size_t rank(value_type x, size_t t) const {
+    if (t == 0) return 0;
+    size_t s = 0;
+    for (size_t i = bitlen; i--;) {
+      size_t j = bitlen-i-1;
+      if (x >> i & 1) {
+        s = zeros[j] + a[j].rank1(s);
+        t = zeros[j] + a[j].rank1(t);
+      } else {
+        s = a[j].rank0(s);
+        t = a[j].rank0(t);
+      }
+    }
+    return t - s;
+  }
+
+  std::pair<bool, value_type> max_lt(value_type x, size_t s, size_t t) const {
+    return max_le(x-1, s, t);
+  }
+  std::pair<bool, value_type> max_le(value_type x, size_t s, size_t t) const {
+    if (s == t) return {false, 0};
+    size_t ri = bitlen+1;
+    size_t rs = -1;
+    size_t rt = -1;
+    bool tight = true;
+    bool reverted = false;
+    value_type res = 0;
+    for (size_t i = bitlen; i--;) {
+      size_t j = bitlen-i-1;
+      size_t z = a[j].rank0(t) - a[j].rank0(s);
+      size_t tg = (tight? (x >> i & 1) : 1);
+      if (reverted) tg = 0;
+
+      bool ok0 = (z > 0);
+      bool ok1 = (z < t-s);
+      size_t ch = 0;
+
+      reverted = false;
+      if (tg == 1) {
+        if (ok0) {
+          ri = i;
+          rs = s;
+          rt = t;
         }
+        if (ok1) {
+          ch = 1;
+        } else {
+          tight = false;
+        }
+      } else if (!ok0 && tight) {
+        if (ri > bitlen) return {false, 0};
+        i = ri+1;
+        s = rs;
+        t = rt;
+        tight = false;
+        value_type mask = (static_cast<value_type>(1) << i) - 1;
+        res |= mask;
+        res ^= mask;
+        reverted = true;
+        continue;
       }
-      bv[i] = BitVector(vb);
 
-      vi = std::move(zero);
-      vi.insert(vi.end(), one.begin(), one.end());  // ???
-
-      if (i <= 3 && false) {
-        for (size_t j=0; j<n; ++j)
-          fprintf(stderr, "%2d%c", vb[j]&1, j+1<n? ' ':'\n');
-
-        for (size_t j=0; j<n; ++j)
-          fprintf(stderr, "%2d%c", vi[j], j+1<n? ' ':'\n');
-
-        fprintf(stderr, "\n");
+      if (ch == 0) {
+        s = a[j].rank0(s);
+        t = a[j].rank0(t);
+      } else {
+        s = zeros[j] + a[j].rank1(s);
+        t = zeros[j] + a[j].rank1(t);
+        res |= static_cast<value_type>(1) << i;
       }
+    }
+    return {true, res};
+  }
+  std::pair<bool, value_type> min_gt(value_type x, size_t s, size_t t) const {
+    return min_ge(x+1, s, t);
+  }
+  std::pair<bool, value_type> min_ge(value_type x, size_t s, size_t t) const {
+    if (s == t) return {false, 0};
+    size_t ri = bitlen+1;
+    size_t rs = -1;
+    size_t rt = -1;
+    bool tight = true;
+    bool reverted = false;
+    value_type res = 0;
+    for (size_t i = bitlen; i--;) {
+      size_t j = bitlen-i-1;
+      size_t z = a[j].rank0(t) - a[j].rank0(s);
+      size_t tg = (tight? (x >> i & 1) : 0);
+      if (reverted) tg = 1;
+
+      bool ok0 = (z > 0);
+      bool ok1 = (z < t-s);
+      size_t ch = 1;
+
+      reverted = false;
+      if (tg == 0) {
+        if (ok1) {
+          ri = i;
+          rs = s;
+          rt = t;
+        }
+        if (ok0) {
+          ch = 0;
+        } else {
+          tight = false;
+        }
+      } else if (!ok1 && tight) {
+        if (ri > bitlen) return {false, 0};
+        i = ri+1;
+        s = rs;
+        t = rt;
+        tight = false;
+        value_type mask = (static_cast<value_type>(1) << ri) - 1;
+        res |= mask;
+        res ^= mask;
+        reverted = true;
+        continue;
+      }
+
+      if (ch == 0) {
+        s = a[j].rank0(s);
+        t = a[j].rank0(t);
+      } else {
+        s = zeros[j] + a[j].rank1(s);
+        t = zeros[j] + a[j].rank1(t);
+        res |= static_cast<value_type>(1) << i;
+      }
+    }
+    return {true, res};
+  }
+
+  value_type quantile(size_t k, size_t s, size_t t) const {
+    value_type res = 0;
+    for (size_t i = bitlen; i--;) {
+      size_t j = bitlen-i-1;
+      size_t z = a[j].rank0(t) - a[j].rank0(s);
+      if (k < z) {
+        s = a[j].rank0(s);
+        t = a[j].rank0(t);
+      } else {
+        res |= static_cast<value_type>(1) << i;
+        s = zeros[j] + a[j].rank1(s);
+        t = zeros[j] + a[j].rank1(t);
+        k -= z;
+      }
+    }
+    return res;
+  }
+
+  std::array<size_t, 3> rank_three_way(value_type x, size_t t) const {
+    if (t == 0) return {0, 0, 0};
+
+    size_t lt = 0;
+    size_t eq = t;
+    size_t gt = 0;
+
+    size_t s = 0;
+    for (size_t i = bitlen; i--;) {
+      size_t j = bitlen-i-1;
+      size_t tmp = (t - s);
+      if (x >> i & 1) {
+        s = zeros[j] + a[j].rank1(s);
+        t = zeros[j] + a[j].rank1(t);
+        size_t d = tmp - (t-s);
+        eq -= d;
+        lt += d;
+      } else {
+        s = a[j].rank0(s);
+        t = a[j].rank0(t);
+        size_t d = tmp - (t-s);
+        eq -= d;
+        gt += d;
+      }
+    }
+    return {lt, eq, gt};
+  }
+
+  std::array<size_t, 3> xored_rank_three_way(value_type x, value_type y, size_t t) const {
+    if (t == 0) return {0, 0, 0};
+
+    size_t lt = 0;
+    size_t eq = t;
+    size_t gt = 0;
+
+    size_t s = 0;
+    for (size_t i = bitlen; i--;) {
+      size_t j = bitlen-i-1;
+      size_t tmp = (t - s);
+      if ((x ^ y) >> i & 1) {
+        s = zeros[j] + a[j].rank1(s);
+        t = zeros[j] + a[j].rank1(t);
+      } else {
+        s = a[j].rank0(s);
+        t = a[j].rank0(t);
+      }
+
+      size_t d = tmp - (t-s);
+      eq -= d;
+      if (y >> i & 1) {
+        lt += d;
+      } else {
+        gt += d;
+      }
+    }
+    return {lt, eq, gt};
+  }
+
+  size_t select(value_type x, size_t t) const {
+    if (t == 0) return 0;
+    size_t si = start_index(x);
+    t += a[bitlen-1].rank(x & 1, si);
+    t = a[bitlen-1].select(x & 1, t);
+
+    for (size_t i = 1; i < bitlen; ++i) {
+      size_t j = bitlen-i-1;
+      if (x >> i & 1) t -= zeros[j];
+      t = a[j].select(x >> i & 1, t);
+    }
+
+    return t;
+  }
+
+  value_type operator [](size_t t) const { return c[t]; }
+
+  void inspect() const {
+    for (size_t i = 0; i < bitlen; ++i) {
+      fprintf(stderr, "%zu (%zu): ", i, zeros[i]);
+      for (size_t j = 0; j < n; ++j)
+        fprintf(stderr, "%d%c", a[i][j], j+1<n? ' ':'\n');
     }
   }
 };
-
-int main() {
-  std::vector<int> a{
-    11, 0, 15, 6, 5, 2, 7, 12,
-    11, 0, 12, 12, 13, 4, 6, 13,
-    1, 11, 6, 1, 7, 10, 2, 7,
-    14, 11, 1, 7, 5, 4, 14, 6
-  };
-  WaveletMatrix<int> wm(a.begin(), a.end());
-}
-
-// std::mt19937 rsk(0315);
-
-// int main() {
-//   std::vector<bool> v(256);
-//   for (size_t i=0; i<v.size(); ++i)
-//     v[i] = rsk() & 1;
-
-//   BitVector bv(v);
-
-//   while (true) {
-//     for (size_t i=0; i<v.size(); ++i) {
-//       if (i % 8 == 0) {
-//         fprintf(stderr, "\x1b[01;31m");
-//       } else if (i % 8 == 4) {
-//         fprintf(stderr, "\x1b[0m");
-//       }
-//       if (i > 0 && i % 64 == 0) {
-//         fprintf(stderr, "\n");
-//       }
-//       fprintf(stderr, "%d", v[i]&1);
-//     }
-//     fprintf(stderr, "\n");
-
-//     char op;
-//     scanf(" %c", &op);
-    
-//     if (op == 's') {
-//       size_t m;
-//       scanf("%zu", &m);
-//       printf("%zu\n", bv.select(m));
-//     } else if (op == 'r') {
-//       size_t k;
-//       scanf("%zu", &k);
-//       printf("%zu\n", bv.rank(k));
-//     }
-//   }
-// }
