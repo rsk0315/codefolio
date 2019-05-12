b9dfef1bcf218771875eab11eae425668f5a1426
rsk0315 <rsk0315@icloud.com>
2019-05-13 01:42:02 +0900

    Add a fix-point utility

---
+++ Utility/fix_point.cpp
@@ -0,0 +1,17 @@
+template <class F>
+class fix_point: F {
+public:
+  explicit constexpr fix_point(F&& f) noexcept: F(std::forward<F>(f)) {}
+
+  template <class... Args>
+  constexpr decltype(auto) operator ()(Args&&... args) const {
+    return F::operator ()(*this, std::forward<Args>(args)...);
+  }
+};
+
+template <class F>
+static inline constexpr decltype(auto) make_fix_point(F&& f) noexcept {
+  return fix_point<F>{std::forward<F>(f)};
+}
+
+make_fix_point([](auto f, int n) { return n? n*f(n-1):1; })(5);
