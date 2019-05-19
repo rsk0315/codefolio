088c346318945447cf4a9e7883cd53638302540a
rsk0315 <rsk0315@icloud.com>
2019-05-19 20:50:55 +0900

    Fix scc

--- Graph/scc.cpp
+++ Graph/scc.cpp
@@ -1,14 +1,14 @@
 template <class Weight>
 std::vector<size_t> strongly_connected_components(const graph<Weight>& g) {
   size_t n = g.size();
-  graph rev(n);
+  graph<int> rev(n);
   for (const auto& v: g)
     for (const auto& e: v)
-      rev.connect_to(e.dst, e.src, e.cost);
+      rev.connect_to(e.dst, e.src);
 
   std::vector<bool> used(n);
   std::vector<size_t> vs;
-  auto dfs = make_fix_point([](auto f, size_t v) {
+  auto dfs = make_fix_point([&](auto f, size_t v) -> void {
       used[v] = true;
       for (size_t i = 0; i < g[v].size(); ++i)
         if (!used[g[v][i].dst]) f(g[v][i].dst);
@@ -19,8 +19,8 @@ std::vector<size_t> strongly_connected_components(const graph<Weight>& g) {
 
   used.assign(n, false);
   std::vector<size_t> cmp(n);
-  num = 0;
-  auto rdfs = make_fix_point([](auto f, size_t v) {
+  size_t num = 0;
+  auto rdfs = make_fix_point([&](auto f, size_t v) -> void {
       used[v] = true;
       cmp[v] = num;
       for (size_t i = 0; i < rev[v].size(); ++i)
