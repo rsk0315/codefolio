8a7d5429f30808041e2777538f2b0ab37bc60063
rsk0315 <rsk0315@icloud.com>
2019-05-21 02:30:44 +0900

    Reduce redundant template arguments

--- Utility/make/vector.cpp
+++ Utility/make/vector.cpp
@@ -1,16 +1,17 @@
-template <size_t Dm, typename Tp>
-std::vector<Tp> make_vector(std::vector<size_t> sizes, typename std::enable_if<(Dm == 1), const Tp&>::type x) {
-  size_t size = 0;
-  if (!sizes.empty()) size = sizes[0];
-  return std::vector<Tp>(size, x);
+template <typename Tp, size_t N>
+std::vector<Tp> make_vector_detail(std::vector<size_t> sizes, typename std::enable_if<(N == 1), const Tp&>::type x) {
+  return std::vector<Tp>(sizes[0], x);
+}
+template <typename Tp, size_t N>
+auto make_vector_detail(std::vector<size_t> sizes, typename std::enable_if<(N > 1), const Tp&>::type x) {
+  size_t size = sizes[N-1];
+  sizes.pop_back();
+  return std::vector<decltype(make_vector_detail<Tp, N-1>(sizes, x))>(size, make_vector_detail<Tp, N-1>(sizes, x));
 }
 
-template <size_t Dm, typename Tp>
-auto make_vector(std::vector<size_t> sizes, typename std::enable_if<(Dm > 1), const Tp&>::type x) {
-  size_t size = 0;
-  if (!sizes.empty()) {
-    size = sizes[0];
-    sizes.erase(sizes.begin());
-  }
-  return std::vector<decltype(make_vector<Dm-1, Tp>(sizes, x))>(size, make_vector<Dm-1, Tp>(sizes, x));
+template <typename Tp, size_t N>
+auto make_vector(const size_t(&sizes)[N], const Tp& x) {
+  std::vector<size_t> s(N);
+  for (size_t i = 0; i < N; ++i) s[i] = sizes[N-i-1];
+  return make_vector_detail<Tp, N>(s, x);
 }
--- Utility/make/vector17.cpp
+++ Utility/make/vector17.cpp
@@ -1,13 +1,17 @@
-template <size_t Dm, typename Tp>
-auto make_vector(std::vector<size_t> sizes, const Tp& x) {
-  size_t size = 0;
-  if (!sizes.empty()) {
-    size = sizes[0];
-    sizes.erase(sizes.begin());
-  }
-  if constexpr (Dm > 1) {
-    return std::vector<decltype(make_vector<Dm-1, Tp>(sizes, x))>(size, make_vector<Dm-1, Tp>(sizes, x));
+template <typename Tp, size_t N>
+auto make_vector_detail(std::vector<size_t> sizes, const Tp& x) {
+  size_t size = sizes[N-1];
+  if constexpr (N > 1) {
+    sizes.pop_back();
+    return std::vector<decltype(make_vector_detail<Tp, N-1>(sizes, x))>(size, make_vector_detail<Tp, N-1>(sizes, x));
   } else {
-    return std::vector<Tp>(x);
+    return std::vector<Tp>(size, x);
   }
+}
+
+template <typename Tp, size_t N>
+auto make_vector(const size_t(&sizes)[N], const Tp& x) {
+  std::vector<size_t> s(N);
+  for (size_t i = 0; i < N; ++i) s[i] = sizes[N-i-1];
+  return make_vector_detail<Tp, N>(s, x);
 }
