93dc84f6ac3ead958756acee31d826ede75b18dc
rsk0315 <rsk0315@icloud.com>
2019-05-24 11:48:33 +0900

    Change style around equal signs

--- Math/mod.cpp
+++ Math/mod.cpp
@@ -2,11 +2,11 @@ template <class Tp>
 Tp gcd(Tp a, Tp b, Tp& x, Tp& y) {
   x = Tp(0);
   y = Tp(1);
-  for (Tp u=y, v=x; a;) {
+  for (Tp u = y, v = x; a;) {
     Tp q = b/a;
-    std::swap(x-=q*u, u);
-    std::swap(y-=q*v, v);
-    std::swap(b-=q*a, a);
+    std::swap(x -= q*u, u);
+    std::swap(y -= q*v, v);
+    std::swap(b -= q*a, a);
   }
   return b;
 }
