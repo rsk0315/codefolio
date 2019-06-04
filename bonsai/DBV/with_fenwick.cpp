9778c1906d37450612f534ebde76dfc542f13d06
rsk0315 <rsk0315@icloud.com>
2019-06-04 20:05:19 +0900

    Add a bit-indexed tree (as a base structure)

---
+++ bonsai/DBV/with_fenwick.cpp
@@ -0,0 +1,76 @@
+#include <cstdio>
+#include <cstdint>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <memory>
+#include <functional>
+
+template <typename Tp, typename Adder = std::plus<Tp>>
+class prefix_sum {
+public:
+  using value_type = Tp;
+  using adder = Adder;
+
+private:
+  std::vector<value_type> M_c;
+
+  void M_build() {
+    size_t n = M_c.size()-1;
+    for (size_t i = 1; i <= n; ++i) {
+      size_t j = i + (i & -i);
+      if (j <= n) M_c[j] += M_c[i];
+    }
+  }
+
+public:
+  prefix_sum() = default;
+  prefix_sum(prefix_sum const&) = default;
+  prefix_sum(prefix_sum&&) = default;
+  prefix_sum(size_t n, value_type const& x = value_type{}): M_c(n+1, x) {
+    M_c[0] = value_type{};
+    M_build();
+  }
+  template <typename InputIt>
+  prefix_sum(InputIt first, InputIt last): M_c(first, last) {
+    M_c.insert(M_c.begin(), value_type{});
+    M_build();
+  }
+
+  void add(size_t i, value_type const& x) {
+    // 1-indexing, internally
+    ++i;
+    size_t n = M_c.size()-1;
+    while (i <= n) {
+      M_c[i] += x;
+      i += (i & -i);
+    }
+  }
+
+  value_type accumulate(size_t i) const {
+    // exclusive in 0-indexing, equivalent to inclusive in 1-indexing
+    value_type res{};
+    while (i > 0) {
+      res += M_c[i];
+      i ^= (i & -i);
+    }
+    return res;
+  }
+
+  void inspect() const {
+    for (size_t i = 1; i < M_c.size(); ++i)
+      fprintf(stderr, "%d%c", M_c[i], i+1<M_c.size()? ' ':'\n');
+  }
+};
+
+int main() {
+  size_t n = 10;
+  std::vector<int> base(n);
+  for (size_t i = 0; i < n; ++i) base[i] = 1 << i;
+  for (size_t i = 0; i < n; ++i)
+    fprintf(stderr, "%d%c", base[i], i+1<n? ' ':'\n');
+  prefix_sum<int> ps(base.begin(), base.end());
+  ps.inspect();
+  for (size_t i = 1; i <= n; ++i)
+    fprintf(stderr, "%d%c", ps.accumulate(i), i<n? ' ':'\n');
+}
