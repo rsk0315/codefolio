b8dc4a494626d020495663f7d1144031de12d954
rsk0315 <rsk0315@icloud.com>
2019-06-06 18:02:41 +0900

    insert

--- bonsai/AVL/avl_tree.cpp
+++ bonsai/AVL/avl_tree.cpp
@@ -1,7 +1,7 @@
 #include <cstdio>
 #include <cstdint>
 #include <cstddef>
-#include <vector>
+#include <cassert>
 #include <algorithm>
 #include <utility>
 #include <memory>
@@ -26,9 +26,12 @@ private:
     value_type value{};
     size_type height = 0;
     using link_type = std::shared_ptr<M_node>;
-    link_type parent = parent;
+    link_type parent = nullptr;
     std::array<link_type, 2> children = {nullptr, nullptr};
     size_type left_size = 0;
+
+    M_node() = default;
+    M_node(value_type const& value): value(value) {}
   };
 
   using link_type = typename M_node::link_type;
@@ -42,6 +45,7 @@ private:
 
 public:
   class iterator {
+    friend avl_tree;
   public:
     using iterator_category = std::random_access_iterator_tag;
     using value_type = typename avl_tree::value_type;
@@ -53,12 +57,13 @@ public:
     using base_ptr = typename avl_tree::base_ptr;
     base_ptr node = nullptr;
 
+    iterator(const_base_ptr const& ptr): node(std::const_pointer_cast<M_node>(ptr)) {}
+
   public:
     iterator() = default;
     iterator(iterator const&) = default;
     iterator(iterator&&) = default;
-    iterator(const_iterator const& other): node(other.node) {}
-    iterator(const_iterator&& other): node(other.node) {}
+    iterator(base_ptr const& ptr): node(ptr) {}
     iterator& operator =(iterator const&) = default;
     iterator& operator =(iterator&&) = default;
 
@@ -82,6 +87,7 @@ public:
   };
 
   class const_iterator {
+    friend avl_tree;
   public:
     using iterator_category = std::random_access_iterator_tag;
     using value_type = typename avl_tree::value_type;
@@ -91,12 +97,18 @@ public:
 
   private:
     using base_ptr = typename avl_tree::const_base_ptr;
+    using mutable_base_ptr = typename avl_tree::base_ptr;
     base_ptr node = nullptr;
 
   public:
     const_iterator() = default;
     const_iterator(const_iterator const&) = default;
     const_iterator(const_iterator&&) = default;
+    const_iterator(iterator const& other): node(std::const_pointer_cast<M_node const>(other.node)) {}
+    const_iterator(iterator&& other): node(std::const_pointer_cast<M_node const>(other.node)) {}
+    const_iterator(base_ptr const& ptr): node(ptr) {}
+    const_iterator(mutable_base_ptr const& ptr): node(std::const_pointer_cast<M_node const>(ptr)) {}
+    
     const_iterator& operator =(const_iterator const&) = default;
     const_iterator& operator =(const_iterator&&) = default;
 
@@ -125,9 +137,30 @@ private:
     return S_balance_factor(std::const_pointer_cast<M_node const>(node));
   }
   static difference_type S_balance_factor(const_base_ptr node) {
-    difference_type left = node->children[0]->height;
-    difference_type right = node->children[1]->height;
-    return left - right;
+    std::array<difference_type, 2> height = {};
+    for (size_t i = 0; i <= 1; ++i)
+      if (node->children[i])
+        height[i] = node->children[i]->height;
+
+    return height[0] - height[1];
+  }
+
+  static void S_fix_height(base_ptr pos) {
+    while (pos) {
+      pos->height = 0;
+      for (auto child: pos->children)
+        if (child) pos->height = 1 + child->height;
+
+      pos = pos->parent;
+    }
+  }
+
+  static void S_fix_left_subtree_size(base_ptr pos, difference_type diff) {
+    while (pos->parent) {
+      if (pos == pos->parent->children[0])
+        pos->parent->left_size += diff;
+      pos = pos->parent;
+    }
   }
 
   void M_prepare_sentinel() {
@@ -166,7 +199,7 @@ private:
       return;
     }
     while (true) {
-      if (pos == pos->parent->chidlren[!dir]) {
+      if (pos == pos->parent->children[!dir]) {
         pos = pos->parent;
         return;
       }
@@ -175,29 +208,29 @@ private:
   }
 
   static void S_advance(const_base_ptr& pos, difference_type count = +1) {
-    if (i > 0) {
+    if (count > 0) {
       while (pos->parent) {
-        difference_type j = i + S_index_diff_to_parent(pos);
-        if (j < 0) break;
-        i = j;
+        difference_type tmp = count + S_index_diff_to_parent(pos);
+        if (tmp < 0) break;
+        count = tmp;
         pos = pos->parent;
       }
-    } else /* if (i < 0) */ {
+    } else /* if (count < 0) */ {
       while (pos->parent) {
-        difference_type j = i + S_index_diff_to_parent(pos);
-        if (j > 0) break;
-        i = j;
+        difference_type tmp = count + S_index_diff_to_parent(pos);
+        if (tmp > 0) break;
+        count = tmp;
         pos = pos->parent;
       }
     }
 
-    while (i != 0) {
-      if (i > 0) {
+    while (count != 0) {
+      if (count > 0) {
         pos = pos->children[1];
-        i -= pos->left_size + 1;
-      } else /* if (i < 0) */ {
+        count -= pos->left_size + 1;
+      } else /* if (count < 0) */ {
         pos = pos->children[0];
-        i += pos->parent->left_size - pos->left_size;
+        count += pos->parent->left_size - pos->left_size;
       }
     }
   }
@@ -220,7 +253,7 @@ private:
   }
 
   static difference_type S_index_diff_to_parent(base_ptr const& pos) {
-    return S_index_diff_to_parent(std::const_base_ptr<M_node const>(pos));
+    return S_index_diff_to_parent(std::const_pointer_cast<M_node const>(pos));
   }
   static difference_type S_index_diff_to_parent(const_base_ptr const& pos) {
     if (pos == pos->parent->children[0]) {
@@ -230,7 +263,7 @@ private:
     }
   }
 
-  static const_base_ptr S_rotate(const_base_ptr pos, size_t dir, base_ptr& root) {
+  static void S_rotate(base_ptr pos, size_t dir, base_ptr& root) {
     base_ptr child = pos->children[!dir];
     pos->children[!dir] = child->children[dir];
     if (child->children[dir])
@@ -252,68 +285,120 @@ private:
     } else {
       pos->left_size -= pos->parent->left_size + 1;
     }
+    S_fix_height(pos);
   }
   void M_rotate(const_base_ptr pos, size_t dir) { S_rotate(pos, dir, M_root); }
 
-  const_base_ptr M_insert(const_base_ptr pos, const_base_ptr newnode) {
+  const_base_ptr M_insert(base_ptr pos, base_ptr newnode) {
     if (pos->children[0]) {
       S_decrement(pos);
       pos->children[1] = newnode;
     } else {
       pos->children[0] = newnode;
-      if (pos == M_begin) M_begin = std::const_pointer_cast<M_node>(pos);
+      if (pos == M_begin) M_begin = newnode;
     }
     newnode->parent = pos;
     ++M_size;
-    // do not forget height
-    M_fix_left_subtree_size(newnode, +1);
+    S_fix_height(pos);
+    S_fix_left_subtree_size(newnode, +1);
     M_postprocess_insert(newnode);
+    return newnode;
   }
-  const_base_ptr M_erase(const_base_ptr pos) {
-    // const? mutable? which is better?
-    const_base_ptr res = pos;
-    S_advance(res);  // XXX
+
+  void M_postprocess_insert(base_ptr pos) { S_postprocess_insert(pos, M_root); }
+  static void S_postprocess_insert(base_ptr pos, base_ptr& root) {
+    base_ptr gchild = pos;
+    base_ptr child = pos;
+    while (pos) {
+      int bf = S_balance_factor(pos);
+      if (!(-1 <= bf && bf <= +1)) break;
+      gchild = child;
+      child = pos;
+      pos = pos->parent;
+    }
+    if (!pos) return;
+
+    if (child == pos->children[0]) {
+      if (gchild == child->children[0]) {
+        // R
+        S_rotate(pos, 1, root);
+      } else {
+        // LR
+        S_rotate(child, 0, root);
+        S_rotate(pos, 1, root);
+      }
+    } else {
+      if (gchild == child->children[0]) {
+        // RL
+        S_rotate(child, 1, root);
+        S_rotate(pos, 0, root);
+      } else {
+        // L
+        S_rotate(pos, 0, root);
+      }
+    }
+  }
+
+  base_ptr M_erase(base_ptr pos) {
     if (pos->children[0] && pos->children[1]) {
       const_base_ptr tmp = pos;
       S_advance(tmp);
       pos->value = std::move(tmp->value);
       pos = tmp;  // pos may have right child
-      // XXX M_end should be taken care of
     }
 
-    // XXX M_begin should be taken care of
-
     base_ptr child = pos->children[0];
-    if (!child) child = pos->children[1];
-    if (child) child->parent = pos->parent;
+    if (!child) {
+      child = pos->children[1];
+      if (pos == M_begin) M_begin = child;
+    }
+
+    if (child) {
+      child->parent = pos->parent;
+    } else if (pos == M_end) {
+      M_end = pos->parent;
+    }
 
     if (!pos->parent) {
       M_root = child;
     } else {
-      M_fix_left_subtree_size(pos, -1);
+      S_fix_left_subtree_size(pos, -1);
       size_type dir = (pos == pos->parent->children[1]);
       pos->parent->children[dir] = child;
+      S_fix_height(pos->parent);
+      M_postprocess_erase(pos->parent);
     }
-
-    // maintain parent-child links appropriately
-    // note that left sizes are also fixed
-
-    // ascend from newnode to M_root
-    // rotate appropriately if unbalanced node is found
   }
 
-  const_base_ptr M_merge(const_base_ptr left, const_base_ptr right);
+  void M_inspect_dfs(const const_base_ptr& root, size_type depth = 0) const {
+    auto child = root->children[1];
+      if (child) M_inspect_dfs(child, depth+1);
+    fprintf(stderr, "%*s-(%p) (%zu): %d\n",
+            static_cast<int>(depth), "",
+            // S_is_red(root)? "\x1b[1;31m":"\x1b[1m",
+            root.get(),
+            // "\x1b[m",
+            root->left_size,
+            root->value
+            );
+    child = root->children[0];
+      if (child) M_inspect_dfs(child, depth+1);
+  }
+
+  const_base_ptr M_merge(base_ptr left, base_ptr right);
   // static const_base_ptr S_merge(const_base_ptr left, const_base_ptr right)
-  const_base_ptr M_split(const_base_ptr pos);
+  const_base_ptr M_split(base_ptr pos);
   // static const_base_ptr S_split(const_base_ptr root, const_base_ptr pos)
 
 public:
   avl_tree() { M_prepare_sentinel(); }
   avl_tree(avl_tree const&) = default;
   avl_tree(avl_tree&&) = default;
-  template <typename InputIt>
-  avl_tree(InputIt first, InputIt last) {
-    M_prepare_sentinel();
+  avl_tree(size_type count, value_type const& value = value_type{}): avl_tree() {
+    while (count--) push_back(value);
+  }
+  template <typename InputIt, typename = std::_RequireInputIter<InputIt>>
+  avl_tree(InputIt first, InputIt last): avl_tree() {
     while (first != last) push_back(*first++);
   }
   avl_tree(std::initializer_list<value_type> ilist): avl_tree(ilist.begin(), ilist.end()) {}
@@ -369,20 +454,20 @@ public:
   void clear() { M_clear(); }
   // ## insert and emplace
   iterator insert(const_iterator pos, value_type const& value) {
-    return M_insert(pos.node, const_base_ptr(new M_node(value)));
+    return M_insert(std::const_pointer_cast<M_node>(pos.node), base_ptr(new M_node(value)));
   }
   iterator insert(const_iterator pos, value_type&& value) {
-    return M_insert(pos.node, const_base_ptr(new M_node(std::move(value))));
+    return M_insert(std::const_pointer_cast<M_node>(pos.node), base_ptr(new M_node(std::move(value))));
   }
   iterator insert(const_iterator pos, size_t count, value_type const& value) {
     iterator res = pos;
-    while (count--) res = M_insert(pos.node, const_base_ptr(new M_node(value)));
+    while (count--) res = M_insert(pos.node, base_ptr(new M_node(value)));
     return res;
   }
   template <typename InputIt>
   iterator insert(const_iterator pos, InputIt first, InputIt last) {
     iterator res = pos;
-    while (first != last) res = M_insert(pos.node, const_base_ptr(new M_node(*first++)));
+    while (first != last) res = M_insert(pos.node, base_ptr(new M_node(*first++)));
     return res;
   }
   iterator insert(const_iterator pos, std::initializer_list<value_type> ilist) {
@@ -418,4 +503,37 @@ public:
   // ## merge and split
   iterator merge(avl_tree&& other);
   avl_tree split(const_iterator pos);
+
+  // # debug
+  // ## inspect
+  void inspect() const {
+    fprintf(stderr, "root: %p\n", M_root.get());
+    fprintf(stderr, "size: %zu\n", M_size);
+    M_inspect_dfs(std::const_pointer_cast<M_node const>(M_root));
+  }
+
+  void verify() const {
+    fprintf(stderr, "begin: %p\n", M_begin.get());
+    fprintf(stderr, "end: %p\n", M_end.get());
+    for (auto it = begin(); it != end(); ++it) {
+      const_base_ptr node = it.node;
+      fprintf(stderr, "%d\n", node->value);
+    }
+  }
 };
+
+#include <vector>
+#include <numeric>
+
+int main() {
+  std::vector<int> v(10);
+  std::iota(v.begin(), v.end(), 0);
+  avl_tree<int> t(v.begin(), v.end());
+  // avl_tree<int> t(1, 1);
+  t.inspect();
+  for (int i = 10; i <= 20; ++i)
+    t.push_back(i);
+  t.inspect();
+
+  t.verify();
+}
