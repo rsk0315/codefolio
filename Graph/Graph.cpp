ede09223168eef9e5f3a4ac61ee5568e346c9549
rsk0315 <rsk0315@icloud.com>
2019-06-07 17:32:00 +0900

    Fix style

--- Graph/Graph.cpp
+++ Graph/Graph.cpp
@@ -1,31 +1,31 @@
-template <class Weight>
+template <typename Weight>
 struct edge {
   using value_type = Weight;
   size_t src, dst;
   value_type cost;
 
-  edge(size_t src, size_t dst, value_type cost=1):
+  edge(size_t src, size_t dst, value_type cost = 1):
     src(src), dst(dst), cost(cost)
   {}
 
-  bool operator <(const edge<value_type> &rhs) const {
+  bool operator <(edge<value_type> const& rhs) const {
     if (cost != rhs.cost) return cost < rhs.cost;
     if (src != rhs.src) return src < rhs.src;
     return dst < rhs.dst;
   }
 };
 
-template <class Weight>
+template <typename Weight>
 struct graph: public std::vector<std::vector<edge<Weight>>> {
   using value_type = Weight;
   graph(size_t n): std::vector<std::vector<edge<value_type>>>(n) {}
 
-  void connect_with(size_t src, size_t dst, value_type cost=1) {
+  void connect_to(size_t src, size_t dst, value_type cost = 1) {
     (*this)[src].emplace_back(src, dst, cost);
-    (*this)[dst].emplace_back(dst, src, cost);
   }
 
-  void connect_to(size_t src, size_t dst, value_type cost=1) {
-    (*this)[src].emplace_back(src, dst, cost);
+  void connect_with(size_t src, size_t dst, value_type cost = 1) {
+    connect_to(src, dst, cost);
+    connect_to(dst, src, cost);
   }
 };
