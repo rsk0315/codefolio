f964581fa02015e9e5117b00d09f553527235189
rsk0315 <rsk0315@icloud.com>
2019-07-05 12:32:43 +0900

    Fix style

--- utility/make/fix_point.cpp
+++ utility/make/fix_point.cpp
@@ -1,15 +1,15 @@
-template <class F>
+template <typename F>
 class fix_point: F {
 public:
   explicit constexpr fix_point(F&& f) noexcept: F(std::forward<F>(f)) {}
 
-  template <class... Args>
+  template <typename... Args>
   constexpr decltype(auto) operator ()(Args&&... args) const {
     return F::operator ()(*this, std::forward<Args>(args)...);
   }
 };
 
-template <class F>
+template <typename F>
 static inline constexpr decltype(auto) make_fix_point(F&& f) noexcept {
   return fix_point<F>{std::forward<F>(f)};
 }
