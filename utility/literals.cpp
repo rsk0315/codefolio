5e86170da3fc36ea2080563bb0aa3dac16354af5
rsk0315 <rsk0315@icloud.com>
2019-07-10 15:43:56 +0900

    Comment out _td literal

--- utility/literals.cpp
+++ utility/literals.cpp
@@ -1,4 +1,4 @@
 constexpr intmax_t  operator ""_jd(unsigned long long n) { return n; }
 constexpr uintmax_t operator ""_ju(unsigned long long n) { return n; }
 constexpr size_t    operator ""_zu(unsigned long long n) { return n; }
-constexpr ptrdiff_t operator ""_td(unsigned long long n) { return n; }
+// constexpr ptrdiff_t operator ""_td(unsigned long long n) { return n; }
