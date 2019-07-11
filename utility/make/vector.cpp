012d13ee62f29a48216fa5274752ef81c41f2db6
rsk0315 <rsk0315@icloud.com>
2019-07-11 16:17:57 +0900

    Change style

--- utility/make/vector.cpp
+++ utility/make/vector.cpp
@@ -1,17 +1,27 @@
+namespace detail {
 template <typename Tp, size_t N>
-std::vector<Tp> make_vector_detail(std::vector<size_t>& sizes, typename std::enable_if<(N == 1), const Tp&>::type x) {
+std::vector<Tp> make_vector(
+    std::vector<size_t>& sizes,
+    typename std::enable_if<(N == 1), const Tp&>::type x
+) {
   return std::vector<Tp>(sizes[0], x);
 }
 template <typename Tp, size_t N>
-auto make_vector_detail(std::vector<size_t>& sizes, typename std::enable_if<(N > 1), const Tp&>::type x) {
+auto make_vector(
+    std::vector<size_t>& sizes,
+    typename std::enable_if<(N > 1), const Tp&>::type x
+) {
   size_t size = sizes[N-1];
   sizes.pop_back();
-  return std::vector<decltype(make_vector_detail<Tp, N-1>(sizes, x))>(size, make_vector_detail<Tp, N-1>(sizes, x));
+  return std::vector<decltype(make_vector<Tp, N-1>(sizes, x))>(
+      size, make_vector<Tp, N-1>(sizes, x)
+  );
 }
+}  // detail::
 
 template <typename Tp, size_t N>
 auto make_vector(const size_t(&sizes)[N], const Tp& x) {
   std::vector<size_t> s(N);
   for (size_t i = 0; i < N; ++i) s[i] = sizes[N-i-1];
-  return make_vector_detail<Tp, N>(s, x);
+  return detail::make_vector<Tp, N>(s, x);
 }
