de61e09eec7f0e1d4017275e38ec7d85b99fed27
rsk0315 <rsk0315@icloud.com>
2019-08-08 08:07:12 +0900

    Fix the position of `const`

--- utility/make/vector.cpp
+++ utility/make/vector.cpp
@@ -2,14 +2,14 @@ namespace detail {
 template <typename Tp, size_t N>
 std::vector<Tp> make_vector(
     std::vector<size_t>& sizes,
-    typename std::enable_if<(N == 1), const Tp&>::type x
+    typename std::enable_if<(N == 1), Tp const&>::type x
 ) {
   return std::vector<Tp>(sizes[0], x);
 }
 template <typename Tp, size_t N>
 auto make_vector(
     std::vector<size_t>& sizes,
-    typename std::enable_if<(N > 1), const Tp&>::type x
+    typename std::enable_if<(N > 1), Tp const&>::type x
 ) {
   size_t size = sizes[N-1];
   sizes.pop_back();
@@ -20,7 +20,7 @@ auto make_vector(
 }  // detail::
 
 template <typename Tp, size_t N>
-auto make_vector(const size_t(&sizes)[N], const Tp& x) {
+auto make_vector(size_t const(&sizes)[N], Tp const& x) {
   std::vector<size_t> s(N);
   for (size_t i = 0; i < N; ++i) s[i] = sizes[N-i-1];
   return detail::make_vector<Tp, N>(s, x);
