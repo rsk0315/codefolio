deebac0767a4b2d21a08ef5f723f07d3c8a6c1ac
rsk0315 <rsk0315@icloud.com>
2019-08-18 17:57:27 +0900

    Add a KMP searcher

---
+++ DataStructure/knuth_morris_pratt_searcher.cpp
@@ -0,0 +1,60 @@
+template <typename ForwardIt, class BinaryPredicate = std::equal_to<>>
+class knuth_morris_pratt_searcher {
+public:
+  using size_type = size_t;
+  using value_type = typename ForwardIt::value_type;
+
+private:
+  static constexpr size_type S_npos = -1;
+  std::vector<value_type> M_pat;
+  std::vector<size_type> M_fail;
+  BinaryPredicate M_pred;
+
+public:
+  knuth_morris_pratt_searcher(ForwardIt pat_first, ForwardIt pat_last,
+                              BinaryPredicate pred = BinaryPredicate()):
+    M_pat(pat_first, pat_last), M_fail(M_pat.size()+1), M_pred(pred)
+  {
+    M_fail[0] = S_npos;
+    for (size_type i = 0, j = S_npos; i < M_pat.size();) {
+      while (j != S_npos && !pred(M_pat[i], M_pat[j])) j = M_fail[j];
+      ++i, ++j;
+      M_fail[i] = (pred(i < M_pat.size() && M_pat[i], M_pat[j])? M_fail[j]: j);
+    }
+  }
+
+  template <typename ForwardIt2>
+  std::vector<std::pair<ForwardIt2, ForwardIt2>> search_all(
+      ForwardIt2 first, ForwardIt2 last
+  ) const {
+    std::vector<std::pair<ForwardIt2, ForwardIt2>> res;
+    size_type j = 0;
+    for (auto it = first; it != last;) {
+      while (j != S_npos && !M_pred(M_pat[j], *it)) j = M_fail[j];
+      ++it;
+      if (++j == M_pat.size()) {
+        res.emplace_back(std::prev(it, j), it);  // XXX non-linear
+        j = M_fail[j];
+      }
+    }
+    return res;
+  }
+
+  template <typename ForwardIt2>
+  std::pair<ForwardIt2, ForwardIt2> search_first(
+      ForwardIt2 first, ForwardIt2 last
+  ) const {
+    size_type j = 0;
+    for (auto it = first; it != last;) {
+      while (j != S_npos && !M_pred(M_pat[j], *it)) j = M_fail[j];
+      ++it;
+      if (++j == M_pat.size()) return {std::prev(it, j), it};
+    }
+    return {last, last};
+  }
+
+  template <typename ForwardIt2>
+  std::pair<ForwardIt2, ForwardIt2> operator ()(
+      ForwardIt2 first, ForwardIt2 last
+  ) const { return search_first(first, last); }
+};
---
+++ bonsai/test/knuth_morris_pratt_searcher.cpp
@@ -0,0 +1,47 @@
+#include <cstdio>
+#include <cstdint>
+#include <cstddef>
+#include <cassert>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <functional>
+
+#include "../../DataStructure/knuth_morris_pratt_searcher.cpp"
+
+void test(std::string const& pat, std::string const& text,
+          std::vector<ptrdiff_t> const& expected) {
+
+  knuth_morris_pratt_searcher kmp(pat.begin(), pat.end());
+
+  auto it = std::search(text.begin(), text.end(), kmp);
+  // auto it = std::search(text.begin(), text.end(), std::default_searcher(pat.begin(), pat.end()));
+  if (it != text.end()) {
+    printf("%td (%s)\n", it-text.begin(), std::string(it, it+pat.length()).c_str());
+    assert(it-text.begin() == expected[0]);
+  } else {
+    printf("not found\n");
+    assert(expected.empty());
+  }
+
+  std::vector<ptrdiff_t> idx;
+  for (auto its: kmp.search_all(text.begin(), text.end())) {
+    std::string s(its.first, its.second);
+    printf("%s (%td)\n", s.c_str(), its.first-text.begin());
+    idx.push_back(its.first-text.begin());
+  }
+  assert(idx == expected);
+}
+
+void testset() {
+  test("abac", "ababacabac", {2, 6});
+  test("aaaa", "aaaaaaaaaabaaaaab", {0, 1, 2, 3, 4, 5, 6, 11, 12});
+  test("abab", "abababcababab", {0, 2, 7, 9});
+  test("aabaabaaa", "aabaabaabaabaaabaabaaaaa", {6, 13});
+  test("xxx", "a", {});
+  test("aab", "abaacabaa", {});
+}
+
+int main() {
+  testset();
+}
