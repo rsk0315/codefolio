7a7a1904027b1aeb66000d39d956b0b6534672d4
rsk0315 <rsk0315@icloud.com>
2019-08-21 15:32:48 +0900

    Add a dual seg-tree

--- DataStructure/dual_segment_tree.cpp
+++ DataStructure/dual_segment_tree.cpp
@@ -23,14 +23,20 @@ public:
   dual_segment_tree(dual_segment_tree const&) = default;
   dual_segment_tree(dual_segment_tree&&) = default;
 
-  dual_segment_tree(size_type n, first_type const& x = binary_operation().identity):
+  dual_segment_tree(size_type n):
     M_base_size(n),
     M_op1(binary_operation()),
     M_op2(external_binary_operation()),
-    M_c(n+n, x)
+    M_c(n+n, M_op1.identity)
+  {}
+
+  dual_segment_tree(size_type n, first_type const& x):
+    M_base_size(n),
+    M_op1(binary_operation()),
+    M_op2(external_binary_operation()),
+    M_c(n+n, M_op1.identity)
   {
-    for (size_type i = n; i--;)
-      M_c[i] = M_op1(M_c[i<<1|0], M_c[i<<1|1]);
+    modify(0, n, x);
   }
 
   template <typename InputIt>
@@ -42,18 +48,30 @@ public:
   {
     for (size_type i = M_base_size; first != last; ++i)
       M_c[i] = *first++;
-    for (size_type i = M_base_size; i--;)
-      M_c[i] = M_op1(M_c[i<<1|0], M_c[i<<1|1]);
   }
 
   dual_segment_tree& operator =(dual_segment_tree const&) = default;
   dual_segment_tree& operator =(dual_segment_tree&&) = default;
 
   void modify(size_type l, size_type r, second_type const& x) {
-    
+    l += M_base_size;
+    r += M_base_size;
+    while (l < r) {
+      if (l & 1) M_c[l] = M_op2(std::move(M_c[l++]), x);
+      if (r & 1) M_c[r] = M_op2(std::move(M_c[--r]), x);
+      l >>= 1;
+      r >>= 1;
+    }
   }
 
-  first_type get(size_type i) const {
+  value_type get(size_type i) const {
+    i += M_base_size;
+    value_type res = M_c[i];
+    while (i > 1) {
+      i >>= 1;
+      res = M_op1(std::move(res), M_c[i]);
+    }
+    return res;
   }
 };
 
