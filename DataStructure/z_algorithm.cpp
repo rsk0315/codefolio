1d164153298a80ef0d853e3bf3513162f8092876
rsk0315 <rsk0315@icloud.com>
2019-08-28 20:26:56 +0900

    Ok, ok (but still requires RandomIt)

--- DataStructure/z_algorithm.cpp
+++ DataStructure/z_algorithm.cpp
@@ -47,90 +47,11 @@ public:
       return res;
     }
 
-#if 0
-    std::vector<size_type> z = M_z;
-    for (size_type j = std::distance(first, last); j--;) z.push_back(0);
-    std::vector<value_type> pat = M_pat;
-    pat.insert(pat.end(), first, last);
-    for (size_type i = M_pat.size(), j = 0; i < pat.size();) {
-      while (i+j < pat.size() && j < M_pat.size() && M_pred(pat[j], pat[i+j])) ++j;
-      z[i] = j;
-      if (j == 0) {
-        ++i;
-        continue;
-      }
-      size_type k = 1;
-      while (i+k < pat.size() && k < M_pat.size() && k+M_z[k] < j) z[i+k] = M_z[k], ++k;
-      i += k;
-      j -= k;
-    }
-    for (size_type i = 0; i < z.size(); ++i)
-      std::cerr << z[i] << (i+1<z.size()? ' ': '\n');
-    for (size_type i = 0; i < pat.size(); ++i)
-      std::cerr << pat[i] << (i+1<pat.size()? ' ': '\n');
-    for (size_type i = M_pat.size(); i < z.size(); ++i) {
-      if (z[i] == M_pat.size())
-        res.emplace_back(first+(i-M_pat.size()), first+(i-M_pat.size()+z[i]));
-    }
-#elif 0
-    std::vector<value_type> pat(first, last);
-    size_type ps = pat.size();
-    std::vector<size_type> z(ps);
-    for (size_type i = 0, j = 0; i < ps;) {
-      while (i+j < pat.size() && j < M_pat.size() && M_pred(M_pat[j], pat[i+j])) ++j;
-      z[i] = j;
-      if (j == 0) {
-        ++i;
-        continue;
-      }
-      size_type k = 1;
-      while (i+k < ps && k < M_pat.size() && k+M_z[k] < j) z[i+k] = M_z[k], ++k;
-      i += k;
-      j -= k;
-    }
-    for (size_type i = 0; i < z.size(); ++i)
-      std::cerr << z[i] << (i+1<z.size()? ' ': '\n');
-    for (size_type i = 0; i < pat.size(); ++i)
-      std::cerr << pat[i] << (i+1<pat.size()? ' ': '\n');
-    for (size_type i = 0; i < z.size(); ++i) {
-      if (z[i] == M_pat.size())
-        res.emplace_back(first+i, first+(i+z[i]));
-    }
-#elif 0
-    std::vector<value_type> pat(first, last);
-    size_type ps = pat.size();
-    std::vector<size_type> z(ps);
     auto it = first;
-    for (size_type i = 0, j = 0; i < ps;) {
-      while (i+j < pat.size() && j < M_pat.size() && M_pred(M_pat[j], *it)) ++j, ++it;
-      z[i] = j;
-      if (j == 0) {
-        ++i;
-        ++it;
-        continue;
-      }
-      size_type k = 1;
-      while (i+k < ps && k < M_pat.size() && k+M_z[k] < j) z[i+k] = M_z[k], ++k;
-      i += k;
-      j -= k;
-    }
-    for (size_type i = 0; i < z.size(); ++i)
-      std::cerr << z[i] << (i+1<z.size()? ' ': '\n');
-    for (size_type i = 0; i < pat.size(); ++i)
-      std::cerr << pat[i] << (i+1<pat.size()? ' ': '\n');
-    for (size_type i = 0; i < z.size(); ++i) {
-      if (z[i] == M_pat.size())
-        res.emplace_back(first+i, first+(i+z[i]));
-    }
-#elif 1
-    std::vector<value_type> pat(first, last);
-    size_type ps = pat.size();
-    std::vector<size_type> z(ps);
-    auto it = first;
-    for (size_type i = 0, j = 0; i < ps;) {
-      while (i+j < pat.size() && j < M_pat.size() && M_pred(M_pat[j], *it)) ++j, ++it;
+    for (size_type i = 0, j = 0; it != last;) {
+      while (it != last && j < M_pat.size() && M_pred(M_pat[j], *it)) ++j, ++it;
       if (j == M_pat.size()) {
-        res.emplace_back(first+i, first+(i+M_pat.size()));
+        res.emplace_back(first+i, it);
       }
       if (j == 0) {
         ++i;
@@ -138,53 +59,10 @@ public:
         continue;
       }
       size_type k = 1;
-      while (i+k < ps && k < M_pat.size() && k+M_z[k] < j) {
-        if (M_z[k] == M_pat.size()) {
-          res.emplace_back(first+i+k, first+(i+k+M_z[k]));
-        }
-        ++k;
-      }
+      while (k < M_pat.size() && k+M_z[k] < j) ++k;
       i += k;
       j -= k;
     }
-    // for (size_type i = 0; i < z.size(); ++i)
-    //   std::cerr << z[i] << (i+1<z.size()? ' ': '\n');
-    // for (size_type i = 0; i < pat.size(); ++i)
-    //   std::cerr << pat[i] << (i+1<pat.size()? ' ': '\n');
-    // for (size_type i = 0; i < z.size(); ++i) {
-    //   if (z[i] == M_pat.size())
-    //     res.emplace_back(first+i, first+(i+z[i]));
-    // }
-#else
-    std::deque<ForwardIt2> start;
-    std::vector<size_type> z(std::distance(first, last));
-    size_type i = 0;
-    size_type j = 0;
-    for (auto it = first; it != last;) {
-      auto it0 = std::next(it, j);
-      while (it0 != last && j < M_pat.size() && M_pred(M_pat[j], *it0)) ++it0, ++j;
-      z[i] = j;
-      if (j == 0) {
-        ++i, ++it;
-        continue;
-      }
-      size_type k = 1;
-      // while (i+k < M_pat.size() && k+M_z[k] < j) z[i+k] = M_z[k], ++k, ++it;
-      // while (it != last && k+M_z[k] < j) z[i+k] = M_z[k], ++k, ++it;
-      // i += k-1;
-      while (std::next(it) != last && k < M_pat.size() && k+M_z[k] < j) z[i+k] = M_z[k], ++k, ++it;
-      i += k;
-      j -= k;
-    }
-    for (size_type i = 0; i < z.size(); ++i) {
-      if (z[i] == M_pat.size())
-        res.emplace_back(first+i, first+i+z[i]);
-    }
-    for (auto it = first; it != last;)
-      std::cerr << *it << (++it!=last? ' ': '\n');
-    for (size_type i = 0; i < z.size(); ++i)
-      std::cerr << z[i] << (i+1<z.size()? ' ': '\n');
-#endif
     return res;
   }
 
