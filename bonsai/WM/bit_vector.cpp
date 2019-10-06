892128de909f30d6d0e5b26a3844c6d42acf51c6
rsk0315 <rsk0315@icloud.com>
2019-10-07 03:27:51 +0900

    Bug tsurai

--- bonsai/WM/bit_vector.cpp
+++ bonsai/WM/bit_vector.cpp
@@ -5,8 +5,94 @@ public:
   using difference_type = ptrdiff_t;
 
 private:
-  std::vector<underlying_type> M_c;
   static const size_type S_ws = CHAR_BIT * sizeof(underlying_type);
+  std::vector<underlying_type> M_c;
+  std::vector<size_type> M_r;
+  std::vector<size_type> M_s0, M_s1;
+  std::vector<std::vector<size_type>> M_ss0, M_ss1;
+
+  static size_type S_popcount(underlying_type n) {
+    return __builtin_popcountll(n);
+  }
+
+  static size_type S_least_n_bits(size_type n) {
+    return (1_ju << n) - 1;
+  }
+
+  template <int Bp>
+  static size_type S_rank_small(underlying_type x, size_type n) {
+    if (Bp == 0) x = ~x;
+    return S_popcount(x & S_least_n_bits(n));
+  }
+
+  template <int Bp>
+  static size_type S_select_small(underlying_type x, size_type n) {
+    if (n == 0) return 0;
+    size_type lb = 0;
+    size_type ub = S_ws;
+    while (ub-lb > 1) {
+      size_type mid = (lb+ub) >> 1;
+      ((S_rank_small<Bp>(x, mid) < n)? lb: ub) = mid;
+    }
+    return ub;
+  }
+
+  template <int Bp>
+  size_type M_rank_large(size_type n) const {
+    size_type res = M_r[n];
+    if (Bp == 0) res = n * S_ws - res;
+    return res;
+  }
+
+  template <int Bp>
+  void M_prepare_select(std::vector<bool> const& bv,
+                        std::vector<size_type>& s,
+                        std::vector<std::vector<size_type>>& ss) {
+    size_type z = 0;
+    size_type n = bv.size();
+    s.push_back(0);
+    std::vector<size_type> tmp;
+    for (size_type i = 0; i < n; ++i) {
+      if (bv[i] != Bp) continue;
+      tmp.push_back(i);
+      if (++z == S_ws) {
+        size_type len = i+1 - s.back();
+        s.push_back(i+1);
+        ss.emplace_back(0);
+        if (len >= S_ws * S_ws) ss.back() = std::move(tmp);
+        tmp.clear();
+      }
+    }
+    ss.push_back(std::move(tmp));
+  }
+
+  size_type M_rank1(size_type t) const {
+    size_type j0 = t / S_ws;
+    size_type j1 = t % S_ws;
+    return M_r[j0] + S_popcount(S_least_n_bits(j1) & M_c[j0]);
+  }
+
+  template <int Bp>
+  size_type M_select(size_type n,
+                     std::vector<size_type> const& s,
+                     std::vector<std::vector<size_type>> const& ss) const {
+
+    if (n-- == 0) return 0;
+    size_type j0 = n / S_ws;
+    size_type j1 = n % S_ws;
+
+    if (j0 >= s.size()) return -1_zu;
+    if (j0+1 == s.size() && j1 >= ss[j0].size()) return -1_zu;
+    if (!ss[j0].empty()) return ss[j0][j1];
+
+    size_type lb = s[j0] / S_ws;
+    size_type ub = (j0+1 < s.size())? (s[j0+1]+S_ws-1) / S_ws: M_r.size();
+    while (ub-lb > 1) {
+      size_type mid = (lb+ub) >> 1;
+      ((M_rank_large<Bp>(mid) <= n)? lb: ub) = mid;
+    }
+    return lb * S_ws + S_select_small<Bp>(s[lb], n - M_rank_large<Bp>(lb));
+  }
 
 public:
   bit_vector() = default;
@@ -21,22 +107,39 @@ public:
   template <typename InputIt>
   void assign(InputIt first, InputIt last) {
     std::vector<bool> tmp(first, last);
-    M_c.resize((tmp+S_ws-1) / S_ws);
+    M_c.resize(tmp.size() / S_ws + 1);
     for (size_type i = 0; i < tmp.size(); ++i) {
+      if (!tmp[i]) continue;
       size_type j0 = i / S_ws;
       size_type j1 = i % S_ws;
       M_c[j0] |= 1_ju << j1;
     }
+
+    // rank
+    M_r.assign(M_c.size(), 0);
+    for (size_type i = 1; i < M_c.size(); ++i)
+      M_r[i] += M_r[i-1] + S_popcount(M_c[i-1]);
+
+    // select
+    M_prepare_select<0>(tmp, M_s0, M_ss0);
+    M_prepare_select<1>(tmp, M_s1, M_ss1);
   }
 
-  size_type rank0(size_type s, size_type t) const;
-  size_type rank1(size_type s, size_type t) const;
-
-  size_type select0(size_type s, size_type t) const;
-  size_type select1(size_type s, size_type t) const;
-
-  // 愚直ににぶたんして O(log n)-time select なのはよくなさそう．
-  // 定数倍の軽めの O(log log n)-time とかができるとうれしい．
+  size_type rank0(size_type s, size_type t) const {
+    return (t-s) - rank1(s, t);
+  }
+  size_type rank1(size_type s, size_type t) const {
+    if (s == t) return 0;
+    return M_rank1(t) - M_rank1(s);
+  }
+  size_type select0(size_type n, size_type s) const {
+    n += rank0(0, s);
+    return M_select<0>(n, M_s0, M_ss0);
+  }
+  size_type select1(size_type n, size_type s) const {
+    n += rank1(0, s);
+    return M_select<1>(n, M_s1, M_ss1);
+  }
 };
 
 template <size_t Nb, typename Tp = uintmax_t>
---
+++ bonsai/test/bit_vector.cpp
@@ -0,0 +1,60 @@
+#include <cstdio>
+#include <cstdint>
+#include <cstddef>
+#include <climits>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <array>
+
+constexpr intmax_t  operator ""_jd(unsigned long long n) { return n; }
+constexpr uintmax_t operator ""_ju(unsigned long long n) { return n; }
+constexpr size_t    operator ""_zu(unsigned long long n) { return n; }
+// constexpr ptrdiff_t operator ""_td(unsigned long long n) { return n; }
+
+#include "../WM/bit_vector.cpp"
+
+#include <cassert>
+#include <set>
+#include <random>
+
+std::mt19937 rsk(0315);
+
+void random_test() {
+  size_t n = 1 << 17;
+  std::vector<bool> vb(n);
+  std::uniform_int_distribution<int> neko(0, 1);
+  std::set<size_t> select;
+  std::vector<size_t> rank(n+1);
+  for (size_t i = 0; i < n; ++i) {
+    rank[i+1] = rank[i];
+    if (!neko(rsk)) continue;
+    vb[i] = true;
+    select.insert(i);
+    ++rank[i+1];
+  }
+
+  bit_vector bv(vb.begin(), vb.end());
+  if (false) {
+    for (size_t i = 0; i <= n; ++i) {
+      fprintf(stderr, "rank[%zu]: %zu, bv.rank1(0, %zu): %zu\n",
+              i, rank[i], i, bv.rank1(0, i));
+      assert(rank[i] == bv.rank1(0, i));
+    }
+  }
+
+  if (true) {
+    size_t i = 1;
+    for (auto s: select) {
+      size_t t = bv.select1(i, 0);
+      fprintf(stderr, "select[%zu]: %zu, bv.select1(%zu, 0): %zu\n",
+              i, s, i, t);
+      ++i;
+      if (i == 1000) break;
+    }
+  }
+}
+
+int main() {
+  random_test();
+}
