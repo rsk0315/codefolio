2d291be1da4c718f47aad0dde5a5afbeddbe796d
rsk0315 <rsk0315@icloud.com>
2019-10-09 18:17:40 +0900

    Add a template

--- algorithm/slide_min.cpp
+++ algorithm/slide_min.cpp
@@ -1,9 +1,10 @@
-template <typename RandomIt, typename OutputIt>
-OutputIt window_minima(RandomIt first, RandomIt s_last, RandomIt last, OutputIt d_first) {
+template <typename RandomIt, typename OutputIt, typename Compare = std::less<>>
+OutputIt window_minima(RandomIt first, RandomIt s_last, RandomIt last,
+                       OutputIt d_first, Compare comp = Compare()) {
   std::deque<RandomIt> dq;
-  ptrdiff_t width = std::distance(first, s_last);
+  ptrdiff_t width = s_last - first;
   for (auto it = first; it < last; ++it) {
-    while (!dq.empty() && *it < *dq.back()) dq.pop_back();
+    while (!dq.empty() && comp(*it, *dq.back())) dq.pop_back();
     dq.push_back(it);
     if (width <= it - dq.front()) dq.pop_front();
     if (width-1 <= it - first) *d_first++ = *dq.front();
