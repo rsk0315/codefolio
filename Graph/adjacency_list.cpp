ea4c338e2192e1d16073a773472222acd3baa234
rsk0315 <rsk0315@icloud.com>
2019-10-13 16:32:48 +0900

    Fix for directedness

--- Graph/adjacency_list.cpp
+++ Graph/adjacency_list.cpp
@@ -32,51 +32,16 @@ public:
   weight_type weight() const { return M_weight; }
 };
 
-template <typename WeightType>
-class colored_weighted_edge: public weighted_edge<WeightType> {
-public:
-  using size_type = size_t;
-  using weight_type = WeightType;
-  using color_type = size_type;
-
-protected:
-  color_type M_color;
-
-public:
-  colored_weighted_edge() = default;
-  colored_weighted_edge(colored_weighted_edge const&) = default;
-  colored_weighted_edge(colored_weighted_edge&&) = default;
-
-  colored_weighted_edge(size_type s, size_type d, color_type c, weight_type w):
-    weighted_edge<WeightType>(s, d, w), M_color(c)
-  {}
-
-  colored_weighted_edge& operator =(colored_weighted_edge const&) = default;
-  colored_weighted_edge& operator =(colored_weighted_edge&&) = default;
-
-  bool operator <(colored_weighted_edge const& other) const {
-    if (this->M_weight < other.M_weight) return true;
-    if (other.M_weight < this->M_weight) return false;
-    if (M_color != other.M_color) return M_color < other.M_color;
-    if (this->M_src != other.M_src) return this->M_src < other.M_src;
-    return this->M_dst < other.M_dst;
-  }
-
-  size_type color() const { return M_color; }
-};
-
 struct directed_tag {};
 struct undirected_tag {};
 
-template <typename Edge, typename Tag>
-class adjacency_list {};
-
-template <typename Edge>
-class adjacency_list<Edge, undirected_tag> {
+template <typename Edge, typename Directedness>
+class adjacency_list {
 public:
   using size_type = size_t;
   using edge_type = Edge;
   using weight_type = typename Edge::weight_type;
+  static weight_type const inf = std::numeric_limits<weight_type>::max();
 
 private:
   std::vector<std::vector<edge_type>> M_g;
@@ -90,7 +55,8 @@ public:
   template <typename... Args>
   void emplace(size_type src, size_type dst, Args... args) {
     M_g[src].emplace_back(src, dst, args...);
-    M_g[dst].emplace_back(dst, src, args...);
+    if (std::is_same<Directedness, undirected_tag>::value)
+      M_g[dst].emplace_back(dst, src, args...);
   }
 
   size_type size() const { return M_g.size(); }
