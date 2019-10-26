a92674bfacf3d2b14b20af3d25cc3a6b90cae498
rsk0315 <rsk0315@icloud.com>
2019-10-26 21:59:47 +0900

    Impl (maybe) tsurakunai functions

--- bonsai/FH/fibonacci_heap.cpp
+++ bonsai/FH/fibonacci_heap.cpp
@@ -1,14 +1,89 @@
-template <typename Tp, typename Compare = std::less<>>
+template <typename Key, typename Tp, typename Compare = std::less<>>
 class fibonacci_heap {
 public:
   using size_type = size_t;
-  using difference_type = ptrdiff_t;
-  using value_type = Tp;
-  using reference = Tp&;
-  using const_reference = Tp const&;
-  using compare = Compare;
+  using key_type = Key;
+  using mapped_type = Tp;
+  using value_type = std::pair<key_type, mapped_type>;
+  using reference = value_type&;
+  using const_reference = value_type const&;
+  using key_compare = Compare;
+  class node_handle;
 
 private:
+  class node;
+  using pointer = std::shared_ptr<node>;
+
+  class node {
+  public:
+    using size_type = size_t;
+    using key_type = Key;
+    using mapped_type = Tp;
+    using value_type = std::pair<key_type, mapped_type>;
+    using reference = value_type&;
+    using const_reference = value_type const&;
+    using key_compare = Compare;
+
+  private:
+    pointer M_parent{nullptr};
+    pointer M_child{nullptr};
+    pointer M_left{nullptr};
+    pointer M_right{nullptr};
+    bool M_damaged = false;
+    size_type M_order = 0;
+    value_type M_value;  // {priority, element}
+
+  public:
+    node() = default;
+    node(node const&) = default;
+    node(node&&) = default;
+    node& operator =(node const&) = default;
+    node& operator =(node&&) = default;
+
+    node(key_type const& key, mapped_type const& mapped):
+      M_value(key, mapped) {}
+  };
+
+public:
+  class node_handle {
+  public:
+    using size_type = size_t;
+    using key_type = Key;
+    using mapped_type = Tp;
+    using value_type = std::pair<key_type, mapped_type>;
+    using reference = value_type&;
+    using const_reference = value_type const&;
+    using key_compare = Compare;
+    using pointer = std::weak_ptr<node>;
+
+  private:
+    pointer M_node;
+
+  public:
+    const_reference operator *() const { return M_node->M_value; }
+  };
+
+private:
+  size_type M_size = 0;
+  pointer M_top{nullptr};
+  std::list<pointer> M_roots;  // list for O(1) meld
+  key_compare M_comp = key_compare();
+
+  static void S_deep_copy_dfs(pointer& dst, pointer& src) {
+    // ???
+  }
+
+  void M_deep_copy(fibonacci_heap const& other) {
+    M_size = other.M_size;
+    M_roots.clear();
+    for (auto const& root: other.M_roots) {
+      pointer r;
+      S_deep_copy_dfs(root);
+      if (root == other.M_top) M_top = r;
+    }
+  }
+
+  void M_coleasce();
 
 public:
   fibonacci_heap() = default;
@@ -23,14 +98,27 @@ public:
   fibonacci_heap(InputIt first, InputIt last) { assign(first, last); }
   fibonacci_heap(std::initializer_list<value_type> il) { assign(ilist); }
 
-  const_reference top() const;
-  void pop();
-  node_handle push(const_reference key);
-  node_handle push(value_type&& key);
+  size_type size() const noexcept { return M_size; }
+  [[nodiscard]] bool empty() const noexcept { return M_size == 0; }
 
-  template <typename... Args>
-  node_handle emplace(Args...&& args);
+  const_reference const& top() const { return M_top->M_value; }
+  void pop();
+  node_handle push(key_type const& key, mapped_type const& mapped) {
+    ++M_size;
+    pointer newnode = std::make_shared<node>(key, mapped);
+    M_roots.push_back(newnode);
+    if (M_comp(M_top->M_value.first, key)) M_top = newnode;
+  }
+
+  void meld(fibonacci_heap& other) {
+    M_size += other.M_size;
+    other.M_size = 0;
+
+    M_roots.splice(M_roots.end(), other.M_roots);
+    if (M_comp(M_top->M_value.first, other.M_top->M_value.first))
+      M_top = other.M_top;
+  }
 
   // node_handle&? node_handle const&?
-  node_handle update(node_handle& nh, const_reference key);
+  node_handle prioritize(node_handle& nh, mapped_type const& key);
 };
