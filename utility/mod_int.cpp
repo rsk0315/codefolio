8375b6b97dda13802681f169edd6dfabe27394f1
rsk0315 <rsk0315@icloud.com>
2019-12-02 16:33:49 +0900

    Fix mod int

--- utility/mod_int.cpp
+++ utility/mod_int.cpp
@@ -10,31 +10,6 @@ private:
   value_type M_value = 0;
   value_type M_mod;  // runtime value (used if S_mod == 0)
 
-  static constexpr value_type S_normalize(value_type n) {
-    if ((n %= S_mod) < 0) n += S_mod;
-    return n;
-  }
-
-  static constexpr value_type S_normalize(value_type n, value_type m) {
-    if ((n %= m) < 0) n += m;
-    return n;
-  }
-
-  static constexpr value_type S_inv(value_type n) {
-    value_type x = 0;
-    value_type y = 1;
-    value_type a = n;
-    value_type b = S_mod;
-    for (value_type u = y, v = x; a;) {
-      value_type q = b / a;
-      std::swap(x -= q*u, u);
-      std::swap(y -= q*v, v);
-      std::swap(b -= q*a, a);
-    }
-    if ((x %= S_mod) < 0) x += S_mod;
-    return x;
-  }
-
   static constexpr value_type S_inv(value_type n, value_type m) {
     value_type x = 0;
     value_type y = 1;
@@ -46,72 +21,73 @@ private:
       std::swap(y -= q*v, v);
       std::swap(b -= q*a, a);
     }
-    if ((x %= S_mod) < 0) x += S_mod;
+    if ((x %= m) < 0) x += m;
     return x;
+  }
+
+  static value_type S_normalize(value_type n, value_type m) {
+    if (n >= m) {
+      n %= m;
+    } else if (n < 0) {
+      n %= m;
+      n += m;
+    }
+    return n;
   }
 
 public:
   modint() = default;
   modint(modint const&) = default;
   modint(modint&&) = default;
-  modint(value_type n): M_value(S_normalize(n)) {}
-  modint(value_type n, value_type m): M_value(S_normalize(n, m)), M_mod(m) {}
+  template <typename Up = Tp, typename std::enable_if<(Modulo > 0), Up>::type* = nullptr>
+  modint(value_type n):
+    M_value(S_normalize(n, Modulo)) {}
+  template <typename Up = Tp, typename std::enable_if<(Modulo == 0), Up>::type* = nullptr>
+  modint(value_type n, value_type m):
+    M_value(S_normalize(n, m)), M_mod(m) {}
+  // copying mod
+  modint(modint const& base, value_type n):
+    M_value(S_normalize(n, base.modulo())), M_mod(base.M_mod) {}
 
   modint& operator =(modint const&) = default;
   modint& operator =(modint&&) = default;
   modint& operator =(value_type n) {
-    M_value = S_normalize(n, S_mod);
+    M_value = S_normalize(n, modulo());
     return *this;
   }
 
   modint& operator +=(modint const& other) {
-    if (S_mod > 0) {
-      if ((M_value += other.M_value) >= S_mod) M_value -= S_mod;
-    } else {
-      if ((M_value += other.M_value) >= M_mod) M_value -= M_mod;
-    }
+    if ((M_value += other.M_value) >= modulo()) M_value -= modulo();
     return *this;
   }
   modint& operator -=(modint const& other) {
-    if (S_mod > 0) {
-      if ((M_value -= other.M_value) < 0) M_value += S_mod;
-    } else {
-      if ((M_value -= other.M_value) < 0) M_value += M_mod;
-    }
+    if ((M_value -= other.M_value) < 0) M_value += modulo();
     return *this;
   }
   modint& operator *=(modint const& other) {
-    if (S_mod > 0) {
-      (M_value *= other.M_value) %= S_mod;
-    } else {
-      (M_value *= other.M_value) %= M_mod;
-    }
+    (M_value *= other.M_value) %= modulo();
     return *this;
   }
   modint& operator /=(modint const& other) {
-    if (S_mod > 0) {
-      (M_value *= S_inv(other.M_value)) %= S_mod;
-    } else {
-      (M_value *= S_inv(other.M_value, M_mod)) %= M_mod;
-    }
+    (M_value *= S_inv(other.M_value, modulo())) %= modulo();
     return *this;
   }
 
   modint& operator +=(value_type const& n) {
-    if (S_mod > 0) return (*this += modint(n));
-    return (*this += modint(n, M_mod));
+    if ((M_value += S_normalize(n, modulo())) >= modulo()) M_value -= modulo();
+    return *this;
   }
   modint& operator -=(value_type const& n) {
-    if (S_mod > 0) return (*this -= modint(n));
-    return (*this -= modint(n, M_mod));
+    if ((M_value -= S_normalize(n, modulo())) < 0) M_value += modulo();
+    return *this;
   }
   modint& operator *=(value_type const& n) {
-    if (S_mod > 0) return (*this *= modint(n));
-    return (*this *= modint(n, M_mod));
+    (M_value *= S_normalize(n, modulo())) %= modulo();
+    return *this;
   }
   modint& operator /=(value_type const& n) {
-    if (S_mod > 0) return (*this /= modint(n));
-    return (*this /= modint(n, M_mod));
+    (M_value *= S_inv(S_normalize(n, modulo()), modulo())) %= modulo();
+    return *this;
   }
 
   modint operator +(modint const& other) const { return modint(*this) += other; }
@@ -125,12 +101,13 @@ public:
 
   modint operator +() const { return *this; }
   modint operator -() const {
-    if (S_mod > 0) return modint(0) -= *this;
-    return modint(0, M_mod) -= *this;
+    if (M_value == 0) return *this;
+    M_value = modulo() - M_value;
+    return *this;
   }
 
   modint pow(intmax_t iexp) const {
-    modint res = ((S_mod > 0)? modint(1): modint(1, M_mod));
+    modint res(*this, 1);
     for (modint dbl = *this; iexp; iexp >>= 1) {
       if (iexp & 1) res *= dbl;
       dbl *= dbl;
@@ -143,7 +120,7 @@ public:
     return M_value == other.M_value;
   }
   bool operator ==(value_type const& n) const {
-    return M_value == S_normalize(n);
+    return M_value == S_normalize(n, modulo());
   }
   bool operator !=(modint const& other) const { return !(*this == other); }
   bool operator !=(value_type const& n) const { return !(*this == n); }
@@ -151,20 +128,25 @@ public:
   value_type get() const { return M_value; }
   value_type modulo() const { return ((S_mod > 0)? S_mod: M_mod); }
 
+  static value_type generator(value_type p) {
+    if (p == 998244353) return 3;
+    return -1;
+  }
+
   std::vector<modint> sqrt() const {
-    intmax_t const p = ((S_mod > 0)? S_mod: M_mod);
+    intmax_t const p = modulo();
     if (p % 4 == 3) {
       modint r = pow((p+1) / 4);
       if (r * r == *this) return {r, -r};
       return {};
     }
 
-    value_type s = __builtin_ctzll(p-1);
+    value_type s = bit::ctz(p-1);
     value_type q = (p-1) >> s;
 
     modint z;
     for (value_type z0 = 2; z0 < p; ++z0) {
-      z = ((S_mod > 0)? modint(z0): modint(z0, M_mod));
+      z = modint(*this, z0);
       if (z.pow((p-1) / 2) == -1) break;
     }
 
@@ -218,3 +200,7 @@ template <typename T1, typename T2, T2 Modulo>
 bool operator !=(T1 const& lhs, modint<T2, Modulo> const& rhs) {
   return !(lhs == rhs);
 }
+
+// constexpr intmax_t mod = 1'000'000'007;  // '
+// constexpr intmax_t mod = 998244353;
+using mi = modint<intmax_t, mod>;
