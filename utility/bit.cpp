3ab83123c6c6be8def71e0c6de1d09892d41edb1
rsk0315 <rsk0315@icloud.com>
2019-12-02 16:33:56 +0900

    Add bit operations

--- utility/bit.cpp
+++ utility/bit.cpp
@@ -4,4 +4,15 @@ int log2p1(Tp n) {
   if (n == 0) return 0;
   return (CHAR_BIT * sizeof(long long)) - __builtin_clzll(n);
 }
+
+template <typename Tp>
+Tp ceil2(Tp n) {
+  if (n == 0) return 1;
+  return Tp{1} << log2p1(n-1);
+}
+
+template <typename Tp>
+int ctz(Tp n) {
+  return __builtin_ctzll(n);
+}
 }  // bit::
