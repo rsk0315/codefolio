3cceb92c409aad665295bf6b0abfdccf477a6c1b
rsk0315 <rsk0315@icloud.com>
2019-12-27 05:22:27 +0900

    neko

--- bonsai/ST/splay_tree.cpp
+++ bonsai/ST/splay_tree.cpp
@@ -37,12 +37,72 @@ private:
     node(node&&) = default;
     node(const_reference value): M_value(value) {}
     node(value_type&& value): M_value(std::move(value)) {}
+
+    void set_size() const {
+      // for splaying
+      M_size = 1;
+      if (M_children[0]) M_size += M_children[0]->M_size;
+      if (M_children[1]) M_size += M_children[1]->M_size;
+    }
+
+    size_type left_size() const {
+      if (!M_children[0]) return 0;
+      return M_children[0]->M_size;
+    }
+    size_type right_size() const {
+      if (!M_children[1]) return 0;
+      return M_children[1]->M_size;
+    }
   };
 
-  static pointer S_next(pointer ptr, difference_type n = 1);
-  static pointer S_prev(pointer ptr, difference_type n = 1);
+  static pointer S_next(pointer ptr, order_statistic_tree* tree,
+                        difference_type n = 1) {
 
-  static size_type S_index(pointer ptr);
+    if (n == 0) return ptr;
+    if (n < 0) return S_prev(ptr, tree, -n);
+
+    S_splay(ptr);
+    ptr = ptr->M_children[1];
+    --n;
+    while (true) {
+      difference_type ls = ptr->left_size();
+      if (n == ls) return ptr;
+      if (n < ls) {
+        ptr = ptr->M_children[0];
+      } else {
+        ptr = ptr->M_children[1];
+        n -= ls+1;
+      }
+    }
+  }
+  static pointer S_prev(pointer ptr, order_statistic_tree* tree,
+                        difference_type n = 1) {
+
+    if (n == 0) return ptr;
+    if (n < 0) return S_next(ptr, tree, -n);
+    if (!ptr)
+      return S_prev(tree->M_rightmost, tree, n-1);  // past-the-end
+
+    S_splay(ptr);
+    ptr = ptr->M_children[0];
+    --n;
+    while (true) {
+      difference_type rs = ptr->right_size();
+      if (n == rs) return ptr;
+      if (n < rs) {
+        ptr = ptr->M_children[1];
+      } else {
+        ptr = ptr->M_children[0];
+        n -= rs+1;
+      }
+    }
+  }
+
+  static size_type S_index(pointer ptr, order_statistic_tree* tree) {
+    if (!ptr) return tree->size();
+    S_splay(ptr);
+    return ptr->left_size();
+  }
 
   static size_type S_parent_dir(pointer ptr) {
     if (!ptr->M_parent) return -1_zu;
@@ -51,11 +111,59 @@ private:
     return -1_zu;
   }
 
-  static pointer S_zig(pointer ptr, size_type dir) {
-    
+  static pointer S_zig(pointer cur, size_type dir) {
+    pointer par = cur->M_parent;
+    pointer sub = cur->M_children[!dir];
+
+    cur->M_children[!dir] = par;
+    par->M_parent = cur;
+    par->M_children[dir] = sub;
+    if (sub) sub->M_parent = par;
+
+    par->set_size();
+    cur->set_size();
+    return cur;
   }
-  static pointer S_zigzig(pointer ptr, size_type dir);
-  static pointer S_zigzag(pointer ptr, size_type dir);
+  static pointer S_zigzig(pointer ptr, size_type dir) {
+    pointer par = cur->M_parent;
+    pointer gpar = par->M_parent;
+    pointer sub1 = cur->M_children[!dir];
+    pointer sub2 = par->M_children[!dir];
+
+    cur->M_children[!dir] = par;
+    par->M_parent = cur;
+    par->M_children[dir] = sub1;
+    if (sub1) = sub1->M_parent = par;
+    par->M_children[!dir] = gpar;
+    gpar->M_parent = par;
+    gpar->M_children[dir] = sub2;
+    if (sub2) = sub2->M_parent = gpar;
+
+    gpar->set_size();
+    par->set_size();
+    cur->set_size();
+    return cur;
+  }
+  static pointer S_zigzag(pointer ptr, size_type dir) {
+    pointer par = cur->M_parent;
+    pointer gpar = par->M_parent;
+    pointer sub1 = cur->M_children[dir];
+    pointer sub2 = cur->M_children[!dir];
+
+    cur->M_children[dir] = gpar;
+    gpar->M_parent = cur;
+    cur->M_children[!dir] = par;
+    par->M_parent = cur;
+    gpar->M_children[!dir] = sub1;
+    if (sub1) sub1->M_parent = gpar;
+    par->M_children[dir] = sub2;
+    if (sub2) sub2->M_parent = par;
+
+    gpar->set_size();
+    par->set_size();
+    cur->set_size();
+    return cur;
+  }
 
   static pointer S_splay(pointer ptr) {
     while (true) {
